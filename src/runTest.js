97c8e7d9ae64bb26c4466d81d1eb56d9f8f83d31
Marco Worms <marcogworms@gmail.com>
2017-03-03 13:06:49 -0300

    this works

---
+++ src/runTest.js
@@ -0,0 +1,5 @@
+module.exports = function () {
+  test('a test', () => {
+    expect(1).toBe(1)
+  })
+}
---
+++ src/something.test.js
@@ -0,0 +1,5 @@
+const runTest = require('./runTest')
+
+describe('something', () => {
+  runTest()
+})
